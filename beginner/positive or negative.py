fcf7203c57acaef2b8a0efcd0da556f6c67ec7ac
333mani <37330826+333mani@users.noreply.github.com>
2018-03-13 16:15:35 +0530

    Update positive or negative.py

--- beginner/positive or negative.py
+++ beginner/positive or negative.py
@@ -1,19 +1,7 @@
-#include <stdio.h>
-int main()
-{
-    double number;
-
-    printf("Enter a number: ");
-    scanf("%lf", &number);
-
-    if (number <= 0.0)
-    {
-        if (number == 0.0)
-            printf("You entered 0.");
-        else
-            printf("You entered a negative number.");
-    }
-    else
-        printf("You entered a positive number.");
-    return 0;
-}
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
